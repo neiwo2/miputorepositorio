07e4712c555ffee8b8b99a5d19c698f4bff56211
Nathan Gutierrez <nathangut98@gmail.com>
2018-05-19 17:49:53 +0200

    Commit 2 Por neiwo

---
+++ miputorepo/src/miputorepo/miputorepo.java
@@ -0,0 +1,10 @@
+package miputorepo;
+
+public class miputorepo {
+
+	public static void main(String[] args) {
+		System.out.println("Mi puto repositorio, te queda claro?");
+		System.out.println("Soy Nathan, hostia");
+	}
+
+}
